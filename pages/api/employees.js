732d82a3421f64aad78fef3d34c2883941e10883
azman <azmanabdlh@yahoo.com>
2022-08-21 15:56:43 +0900

    fix image

--- pages/api/employees.js
+++ pages/api/employees.js
@@ -4,31 +4,31 @@ const employees = [
     "id": 1,
     "name": "drg. WENDY PELUPESSY, M.Kes",
     "employee_role": "KEPALA DINAS ",
-    "profile": "/static/pegawai/diskes/kadis.jpg"
+    "profile": "/static/pegawai/diskes/kadis.JPG"
   },
   {
     "id": 2,
     "name": "dr. TJIANG ROBERTH CHANDRA, M.Kes",
     "employee_role": "SEKERTARIS DINAS",
-    "profile": "/static/pegawai/diskes/sekdis.jpg"
+    "profile": "/static/pegawai/diskes/sekdis.JPG"
   },
   {
     "id": 3,
     "name": "J. RIRIHENA",
     "employee_role": "KEPALA SUBAG KEUANGAN DAN ASET",
-    "profile": "/static/pegawai/diskes/ririhena.jpg"
+    "profile": "/static/pegawai/diskes/ririhena.JPG"
   },
   {
     "id": 4,
     "name": "JAKOBIS LEWIER, S.Sos, M.KM",
     "employee_role": "KEPALA SUBAG PERENCANAAN, KEPEGAWAIAN DAN UMUM",
-    "profile": "/static/pegawai/diskes/jakobis.jpg"
+    "profile": "/static/pegawai/diskes/jakobis.JPG"
   },
   {
     "id": 5,
     "name": "R.J. TALLE SKM, M.Kes",
     "employee_role": "KEPALA BIDANG P2P",
-    "profile": "/static/pegawai/diskes/talle.jpg"
+    "profile": "/static/pegawai/diskes/talle.JPG"
   },
   {
     "id": 6,
@@ -40,61 +40,61 @@ const employees = [
     "id": 7,
     "name": "dr. YUSDA TUHAREA",
     "employee_role": "KEPALA BIDANG KESEHATAN MASYARAKAT",
-    "profile": "/static/pegawai/diskes/yusda.jpg"
+    "profile": "/static/pegawai/diskes/yusda.JPG"
   },
   {
     "id": 8,
     "name": "TIARA URSULA SISWANTO",
     "employee_role": "KONTRAK",
-    "profile": "/static/pegawai/diskes/tiara.jpg"
+    "profile": "/static/pegawai/diskes/tiara.JPG"
   },
   {
     "id": 9,
     "name": "BOKI HAWA USEMAHU, Amd.AK",
     "employee_role": "KONTRAK",
-    "profile": "/static/pegawai/diskes/boki.jpg"
+    "profile": "/static/pegawai/diskes/boki.JPG"
   },
   {
     "id": 10,
     "name": "AMIAMALIA TANGKE, Amd. AK",
     "employee_role": "KONTRAK",
-    "profile": "/static/pegawai/diskes/amiamalia.jpg"
+    "profile": "/static/pegawai/diskes/amiamalia.JPG"
   },
   {
     "id": 11,
     "name": "NABILA PUTRI RISKY, Amd.AK",
     "employee_role": "KONTRAK",
-    "profile": "/static/pegawai/diskes/nabila.jpg"
+    "profile": "/static/pegawai/diskes/nabila.JPG"
   },
   {
     "id": 12,
     "name": "YULIET SOPLANIT, Amd.AK",
     "employee_role": "KONTRAK",
-    "profile": "/static/pegawai/diskes/yuliet.jpg"
+    "profile": "/static/pegawai/diskes/yuliet.JPG"
   },
   {
     "id": 13,
     "name": "YANA SOMAE, Ns",
     "employee_role": "KONTRAK",
-    "profile": "/static/pegawai/diskes/yana.jpg"
+    "profile": "/static/pegawai/diskes/yana.JPG"
   },
   {
     "id": 14,
     "name": "SEMOEL L. SAMSU, ST",
     "employee_role": "KONTRAK",
-    "profile": "/static/pegawai/diskes/semoel.jpg"
+    "profile": "/static/pegawai/diskes/semoel.JPG"
   },
   {
     "id": 15,
     "name": "NOVITA TUHAREA, SE",
     "employee_role": "KONTRAK",
-    "profile": "/static/pegawai/diskes/novita.jpg"
+    "profile": "/static/pegawai/diskes/novita.JPG"
   },
   {
     "id": 16,
     "name": "NETY SOHUAT, S.Kom",
     "employee_role": "KONTRAK",
-    "profile": "/static/pegawai/diskes/nety.jpg"
+    "profile": "/static/pegawai/diskes/nety.JPG"
   },
   {
     "id": 17,
@@ -112,25 +112,25 @@ const employees = [
     "id": 19,
     "name": "ALI ALFATASYAH ARRIFKIH MUH",
     "employee_role": "KONTRAK",
-    "profile": "/static/pegawai/diskes/ali.jpg"
+    "profile": "/static/pegawai/diskes/ali.JPG"
   },
   {
     "id": 20,
     "name": "EVELYN MEISAN THENU",
     "employee_role": "KONTRAK",
-    "profile": "/static/pegawai/diskes/evelyn.jpg"
+    "profile": "/static/pegawai/diskes/evelyn.JPG"
   },
   {
     "id": 21,
     "name": "ZUSIANA NAOMI HAUMAHU",
     "employee_role": "KONTRAK",
-    "profile": "/static/pegawai/diskes/zusiana.jpg"
+    "profile": "/static/pegawai/diskes/zusiana.JPG"
   },
   {
     "id": 22,
     "name": "AGUSTINA HAUMAHU",
     "employee_role": "KONTRAK",
-    "profile": "/static/pegawai/diskes/agustina.jpg"
+    "profile": "/static/pegawai/diskes/agustina.JPG"
   },
   {
     "id": 23,
